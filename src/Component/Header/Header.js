7a954f281b27399aa74b63867022146f61e4cfb5
Farhanayesmin2 <farhanayesmin@gmail.com>
2023-03-31 16:17:24 +0600

    Navbar added

--- src/Component/Header/Header.js
+++ src/Component/Header/Header.js
@@ -1,25 +1,26 @@
 import React from 'react';
+import { GiBowTieRibbon } from "react-icons/gi";
 
 const Header = () => {
     return (
         <div>
-            <div className="navbar bg-violet-200 text-indigo-900 lg:px-14 ">
+            <div className="navbar bg-yellow-50  text-indigo-900 lg:px-14  shadow-md ">
   <div className="navbar-start">
                     <div className="dropdown">
                             
       <label tabIndex={1} className="btn btn-ghost btn-circle">
-                            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h7" /></svg>
+                       <GiBowTieRibbon className='text-xl text-rose-600'></GiBowTieRibbon>
                             
                         </label>
                         
      
-                        <a className="btn btn-ghost normal-case lg:text-3xl text-violet-700 font-serif">Knowledge Gather</a>
+                        <a className="btn btn-ghost normal-case lg:text-2xl text-violet-700 font-serif">Knowledge Gather</a>
     </div>
   </div>
   <div className="navbar-start">
 
   </div>
-                <div className="navbar-end">
+                <div className="navbar-end font-serif font-bold">
                       <ul className=' flex  p-2 text-purple-900' >
                     <li className=' mx-3'  ><a>Home</a></li>
                     <li className=' mx-3' ><a>Blog</a></li>
